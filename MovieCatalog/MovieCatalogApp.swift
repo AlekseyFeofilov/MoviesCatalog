ad307adb2f619f4237c13a4cd6451349c51b1cb2
Feofilov Aleksey <Alexfil888@gmail.com>
2022-10-18 12:43:43 +0700

    Initial Commit

---
+++ MovieCatalog/MovieCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCatalogApp.swift
+//  MovieCatalog
+//
+//  Created by  Aleksey Feofilov on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
