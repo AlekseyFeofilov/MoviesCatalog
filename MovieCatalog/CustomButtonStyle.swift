5a207bef84249ecd19fdafa62da93ecb6d4e12dd
Feofilov Aleksey <alexfil888@gmail.com>
2022-10-24 17:38:35 +0700

    Task №13712 - add custom button style

---
+++ MovieCatalog/CustomButtonStyle.swift
@@ -0,0 +1,50 @@
+//
+//  CustomButton.swift
+//  MovieCatalog
+//
+//  Created by  Aleksey Feofilov on 21.10.2022.
+//
+
+import SwiftUI
+
+struct CustomButtonStyle: ButtonStyle {
+    @State var active: Bool
+
+    func makeBody(configuration: Self.Configuration) -> some View {
+        if(active){
+            configuration.label
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 44)
+                    .background(
+                            RoundedRectangle(cornerRadius: 4)
+                                    .fill(Color.accentColor)
+                    )
+                    .overlay{
+                        RoundedRectangle(cornerRadius: 4)
+                                .stroke(Color("GrayColor"), lineWidth: 0)
+                    }
+        }
+        else {
+            configuration.label
+                    .foregroundColor(.accentColor)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 44)
+                    .overlay{
+                        RoundedRectangle(cornerRadius: 4)
+                                .stroke(Color("GrayColor"), lineWidth: 1)
+                    }
+        }
+    }
+}
+
+struct CustomButton_Previews: PreviewProvider {
+    @State static var active: Bool = true
+
+    static var previews: some View {
+        Button(action: {}){
+            Text("Button")
+        }
+                .buttonStyle(CustomButtonStyle(active: active))
+    }
+}
