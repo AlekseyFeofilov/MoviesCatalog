9a6c949834ccbe2a94f538d8621f5425ff81ab31
Feofilov Aleksey <alexfil888@gmail.com>
2022-11-02 18:36:37 +0700

    add custom async image

---
+++ MovieCatalog/View/CustomAsyncImage.swift
@@ -0,0 +1,39 @@
+//
+//  CustomAsyncImage.swift
+//  MovieCatalog
+//
+//  Created by  Aleksey Feofilov on 30.10.2022.
+//
+
+import SwiftUI
+
+struct CustomAsyncImage: View {
+    var imageStringUrl: String
+    var failureImage: Image
+    
+    var body: some View {
+        AsyncImage(url: URL(string: imageStringUrl)){phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+                
+            case .success(let image):
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                
+            case .failure:
+                failureImage
+                
+            @unknown default:
+                EmptyView()
+            }
+        }
+    }
+}
+
+struct CustomAsyncImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomAsyncImage(imageStringUrl: "https://sun9-43.userapi.com/impf/c854220/v854220563/136392/vCakNgKV8ho.jpg?size=568x608&quality=96&sign=f4deffe9d8c70e8c617d43e8779afff5&type=album", failureImage: Image("ProfileImage"))
+    }
+}
