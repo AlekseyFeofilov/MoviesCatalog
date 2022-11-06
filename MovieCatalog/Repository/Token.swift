c1e31d85eb64e993bcd42926a34028e6ef3a1047
Feofilov Aleksey <alexfil888@gmail.com>
2022-11-06 16:21:05 +0700

    Task №14936 - add useCases for saving and loading token

---
+++ MovieCatalog/Repository/Token.swift
@@ -0,0 +1,28 @@
+//
+//  Token.swift
+//  MovieCatalog
+//
+//  Created by  Aleksey Feofilov on 06.11.2022.
+//
+
+import Foundation
+
+struct Token: Codable {
+    let token: String
+}
+
+func setToken(_ token: String){
+    if let encoded: Data = token.data(using: .utf8) {
+        UserDefaults.standard.set(encoded, forKey: tokenKey)
+    }
+}
+
+func getToken() -> String? {
+    if let data = UserDefaults.standard.data(forKey: tokenKey){
+        if let decoded = String(data: data, encoding: .utf8){
+            return decoded
+        }
+    }
+    
+    return nil
+}
