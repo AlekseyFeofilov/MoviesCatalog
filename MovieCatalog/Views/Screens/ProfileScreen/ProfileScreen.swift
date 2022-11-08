f0bc40c96d2edb7d5ae6ace0e953ae47f9a8657e
Feofilov Aleksey <alexfil888@gmail.com>
2022-11-08 23:43:25 +0700

    Task №13710 - complete ProfileScreen.swift

--- MovieCatalog/Views/Screens/ProfileScreen/ProfileScreen.swift
+++ MovieCatalog/Views/Screens/ProfileScreen/ProfileScreen.swift
@@ -8,78 +8,73 @@
 import SwiftUI
 
 struct ProfileScreen: View {
-    @State var url: String
-    @State var name: String
-    @State var email: String
-    @State var birthday: Date
-    @State var gender: Gender?
+    @ObservedObject var viewModel: ProfileScreenViewModel
     
     var body: some View {
         ZStack{
-            Color("BackgroundColor")
+            Color(backgroundColorName)
+                .ignoresSafeArea()
             
             VStack{
                 HStack(spacing: 16){
-                    CustomAsyncImage(imageStringUrl: url, failureImage: Image(profileImageName))
-                    Text(name)
+                    CustomAsyncImage(imageStringUrl: viewModel.imageUrl, failureImage: Image(profileImageName))
+                        .frame(width: 88, height: 88, alignment: .center)
+                        .clipShape(Circle())
+                    
+                    Text(viewModel.nickName)
                         .foregroundColor(.white)
                         .font(Font.custom("SFProText-Bold", size: 24))
                     Spacer()
                 }
-                .padding(.leading, 16)
                 
+                Spacer()
                 
-                VStack{
-                    HStack {
-                        Text("E-mail")
-                        Spacer()
-                    }
-                    CustomTextField(placeholder: "", text: $email)
+                VStack(alignment: .leading){
+                    Text(emailText)
+                    CustomTextField(placeholder: "", text: $viewModel.email)
+                        .padding(.bottom)
+                    Text("Ссылка на аватарку")
+                    CustomTextField(placeholder: "", text: $viewModel.imageUrl)
+                        .padding(.bottom)
+                    Text(nameText)
+                    CustomTextField(placeholder: "", text: $viewModel.name)
+                        .padding(.bottom)
+                    Text(birthdayText)
+                    DatePickerFieldView(placeholder: "", text: $viewModel.name, savedDate: $viewModel.birthday)
+                        .padding(.bottom)
+                    Text("Пол")
+                    SexButtonView($viewModel.gender)
                 }
-                VStack{
-                    HStack {
-                        Text("Ссылка на аватарку")
-                        Spacer()
-                    }
-                    CustomTextField(placeholder: "", text: $url)
+                .foregroundColor(Color(grayColorName))
+                
+                Spacer()
+                
+                Text(viewModel.profileChangingError.description)
+                    .foregroundColor(.accentColor)
+                    .font(.system(size: 16))
+                    .frame(maxWidth: .infinity, minHeight: 30, alignment: .leading)
+                
+                Button(action: viewModel.changeProfileInfo) {
+                    Text("Сохранить")
                 }
-                VStack{
-                    HStack {
-                        Text("Имя")
-                        Spacer()
+                .padding(.bottom, smallPadding)
+                .buttonStyle(CustomButtonStyle(active: viewModel.isButtonActive))
+                
+                Text("Выйти из аккаунта")
+                    .frame(height: smallFontSize)
+                    .foregroundColor(Color.accentColor)
+                    .onTapGesture {
+                        viewModel.logout()
                     }
-                    CustomTextField(placeholder: "", text: $email)
-                }
-                VStack{
-                    HStack {
-                        Text("Дата рождения")
-                        Spacer()
-                    }
-                    //DatePickerFieldView(placeholder: "", text: $name)
-                }
-                VStack{
-                    HStack {
-                        Text("Пол")
-                        Spacer()
-                    }
-                    SexButtonView($gender)
-                }
+                    .padding(.bottom, smallPadding)
             }
-            .foregroundColor(.white)
-            
+            .padding(.horizontal)
         }
-        .ignoresSafeArea()
     }
 }
 
 struct ProfileScreen_Previews: PreviewProvider {
-    @State static var url: String = "https://sun9-43.userapi.com/impf/c854220/v854220563/136392/vCakNgKV8ho.jpg?size=568x608&quality=96&sign=f4deffe9d8c70e8c617d43e8779afff5&type=album"
-    @State static var name: String = "Lyosha"
-    @State static var email: String = "mail@mail.ru"
-    @State static var birthday: Date = Date.now
-    @State static var gender = Gender.male
-    
     static var previews: some View {
-        ProfileScreen(url: url, name: name, email: email, birthday: birthday, gender: gender)
+        ProfileScreen(viewModel: ProfileScreenViewModel(isAuthorized: .constant(false)))
     }
 }
